2b55eacbd40bb39c9ed96c490ec541a76ece0f67
Efe <dilmacefe@gmail.com>
2023-03-29 20:38:24 +0300

    added some dependencies

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const feathers = require('@feathersjs/feathers');
+const express = require('@feathersjs/express');
 const dotenv = require('dotenv');
-const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const path = require('path');
 
 const connectDB = require('./server/database/connection');
 
-const app = express();
+const app = express(feathers());
 
 dotenv.config( { path : 'config.env'} )
 const PORT = process.env.PORT || 8080
@@ -19,7 +19,6 @@ app.use(bodyparser.urlencoded({ extended : true}))
 
 // set view engine
 app.set("view engine", "ejs")
-//app.set("views", path.resolve(__dirname, "views/ejs"))
 
 // load assets
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
